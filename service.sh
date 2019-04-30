dd33d88812f48cc728a40b816f362a9ae0c18581
Jimmy Figueroa <av_jfigueroa@idcontractors.net>
2019-04-29 18:37:56 -0600

    1st incarnation of top 20 results

--- service.sh
+++ service.sh
@@ -1,3 +1,8 @@
+# remove previous files
 rm -rf big*
 
+# produce a list of most programming languages
 wget https://dzone.com/articles/big-list-256-programming && cat big-list-256-programming  | grep 'nofollow">.*<' | cut -d 
+
+# search google and list top results
+googler --count 10000 preferred hacker programming languages | grep -o '\w*' | grep -Ff languages.txt | sort | uniq -c | sort -r | head -20
