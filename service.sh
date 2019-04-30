60cfba6d091c96b75e7ad53f3fa7048190ab3784
Jimmy Figueroa <av_jfigueroa@idcontractors.net>
2019-04-29 18:46:24 -0600

    version 0 completed

--- service.sh
+++ service.sh
@@ -5,4 +5,4 @@ rm -rf big*
 wget https://dzone.com/articles/big-list-256-programming && cat big-list-256-programming  | grep 'nofollow">.*<' | cut -d 
 
 # search google and list top results
-googler --count 10000 preferred hacker programming languages | grep -o '\w*' | grep -Ff languages.txt | sort | uniq -c | sort -r | head -20
+shell2http -port 8081 /tophacklang "googler --count 10000 preferred hacker programming languages | grep -o '\w*' | grep -Ff languages.txt | sort | uniq -c | sort -r | head -20"
